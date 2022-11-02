0972faae5f89301dfbbc973a848b8c937ffc3cd7
CSH111 <seongho6022@naver.com>
2022-11-02 20:30:06 +0900

    Feat: route nesting 이용 레이아웃 출력

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 
+import { Layout } from "./components/Layout";
 import { Detail, Home } from "./pages";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/:id" element={<Detail />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Detail />} />
+      </Route>
     </Routes>
   );
 }
